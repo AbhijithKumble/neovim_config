09cea02fa6636b0a46fcdd9fb256b98ac72037d1
AbhijithKumble <abhijithkumble87564@gmail.com>
2024-05-09 19:33:03 +0530

    added the remaps from theprimeagen

--- lua/abhijith/lazy/lsp.lua
+++ lua/abhijith/lazy/lsp.lua
@@ -1,10 +1,11 @@
---[[return {
+return {
     --- Uncomment the two plugins below if you want to manage the language 
     --servers from neovim
     "neovim/nvim-lspconfig",
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
@@ -15,30 +16,79 @@
         "j-hui/fidget.nvim",
     },
 
-    config = function() 
+     config = function()
         local cmp = require('cmp')
-        local cmp_lsp = require("cmp-nvim-lsp")
+        local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
             "force",
             {},
             vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabalities())
-    
+            cmp_lsp.default_capabilities())
+
         require("fidget").setup({})
-        require("mason").setup({}) 
+        require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
+                "clangd",
+            },
+            handlers = {
+                function(server_name) -- default handler (optional)
 
-        }, 
-        handlers = {
-            
-        }
-    })
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
 
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+				    runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                }
+                            }
+                        }
+                    }
+                end,
+            }
+        })
+
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
+        vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
     end
-
-
-} ]]--
-
-return {}
+}
--- lua/abhijith/lazy/zenmode.lua
+++ lua/abhijith/lazy/zenmode.lua
@@ -1,9 +1,36 @@
 return {
---[[    "mbbill/undotree",
+    "folke/zen-mode.nvim",
+    config = function()
+        vim.keymap.set("n", "<leader>zz", function()
+            require("zen-mode").setup {
+                window = {
+                    width = 90,
+                    options = { }
+                },
+            }
+            require("zen-mode").toggle()
+            vim.wo.wrap = false
+            vim.wo.number = true
+            vim.wo.rnu = true
+            ColorMyPencils()
+        end)
 
-    config = function() 
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+        vim.keymap.set("n", "<leader>zZ", function()
+            require("zen-mode").setup {
+                window = {
+                    width = 80,
+                    options = { }
+                },
+            }
+            require("zen-mode").toggle()
+            vim.wo.wrap = false
+            vim.wo.number = false
+            vim.wo.rnu = false
+            vim.opt.colorcolumn = "0"
+            ColorMyPencils()
+        end)
     end
-    ]]--
 }
+
 
